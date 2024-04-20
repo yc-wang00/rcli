9b988d15db0a2b8810be0dd497ec0c0f0dd890b5
yc-wang <yicheng.wang.2023@gmail.com>
2024-04-20 15:12:04 +0100

    chore: add a quick rust cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod opts;
+
+use clap::Parser;
+
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(csv_opts) => {
+            process_csv(&csv_opts.input, &csv_opts.output)?;
+        }
+    }
+
+    Ok(())
 }
---
+++ src/opts.rs
@@ -0,0 +1,40 @@
+use std::path::Path;
+
+use clap::Parser;
+
+// rcli csv -i input.csv -o output.json --header -d ','
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author = "Rust CLI", about, long_about =None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to JSON")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser(verify_input_file))]
+    pub input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File does not exist")
+    }
+}
